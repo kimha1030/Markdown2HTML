21ed1c29574a4528fcd9a1e890aec4609e9105a8
kimha1030 <1478@holbertonschool.com>
2020-12-23 13:37:54 -0500

    Create functions to convert md to html

--- markdown2html.py
+++ markdown2html.py
@@ -14,17 +14,22 @@ def markdownhtml():
         filename = sys.argv[1]
         output = sys.argv[2]
         if os.path.isfile(filename):
-            import markdown
-            markdown.markdownFromFile(
-                input=filename,
-                output=output,
-                encoding='utf-8',)
-            with open(output, 'a') as file_output:
-                file_output.write("\n")
+            convert_markdown(filename, output)
             sys.exit(0)
         else:
             print("Missing {}".format(filename), file=sys.stderr)
             sys.exit(1)
+
+
+def convert_markdown(filename, output):
+    """Takes 2 arguments and convert md file to html"""
+    import markdown
+    markdown.markdownFromFile(
+        input=filename,
+        output=output,
+        encoding='utf-8',)
+    with open(output, 'a') as file_output:
+        file_output.write("\n")
 
 
 if __name__ == "__main__":
